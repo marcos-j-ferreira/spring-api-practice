215875d13799f6f620a47137978df9496912f8f5
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-07-19 12:55:01 -0300

    feat: testes de respostas

--- api/src/main/java/com/marcos/api/controllers/Controller.java
+++ api/src/main/java/com/marcos/api/controllers/Controller.java
@@ -1,9 +1,10 @@
-package com.marcos.api;
+package com.marcos.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,25 +13,29 @@ import org.springframework.http.HttpHeaders;
 @RequestMapping("/api/v1/")
 public class Controller{
 
+    //Retorna uma mensagem hello para o usuário
     @GetMapping
     public ResponseEntity<String> hello(){
-        String hello = "{\"messagem\":\"Hello, World!\"}";
+        String hello = "{\"messagem\":\"Hello, World!\"}\n";
 
         return ResponseEntity
                 .status(200)
                 .header("Content-Type","application/json")
                 .body(hello);
-
     }
 
-    @GetMapping("aaa")
-    public ResponseEntity<String> hello01(){
-        String hello = "{\"messagem\":\"Hello, World!\"}";
+    //recebe um nome no query, e retorna um json com nome<nome> do usuário.
+    @GetMapping("param")
+    public ResponseEntity<String> nome(
+        @RequestParam (required=true) String nome
+        ){
+        String corpo = "{\"Nome\":" + "\""+nome+"\"}\n";
 
         return ResponseEntity
                 .status(200)
-                .header("Content-Type","application/json")
-                .body(hello);
-
+                .header("Content-Type", "application/json")
+                .body(corpo);
     }
-}
+
+    
+} 
